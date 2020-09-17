08ebb9b6c8f1179428f2396f5e6ba41f89097963
Glenlivet Zhang <glenlivet.zhang@pwc.com>
2020-09-17 08:29:25 +0800

    init commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Menubar } from "primereact/menubar";
 
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/saga-blue/theme.css";
@@ -9,12 +12,63 @@ import LoginForm from "./LoginForm";
 
 import "./App.scss";
 
+const items = [
+  {
+    label: "Options",
+    items: [
+      {
+        label: "New",
+        icon: "pi pi-fw pi-plus",
+        command: () => {
+          window.location.hash = "/fileupload";
+        },
+      },
+      {
+        label: "Delete",
+        icon: "pi pi-fw pi-trash",
+        url: "http://primetek.com.tr",
+      },
+    ],
+  },
+  {
+    label: "Account",
+    items: [
+      {
+        label: "Options",
+        icon: "pi pi-fw pi-cog",
+        command: () => {
+          window.location.hash = "/";
+        },
+      },
+      { label: "Sign Out", icon: "pi pi-fw pi-power-off" },
+    ],
+  },
+];
+
+function Dashboard() {
+  return <div>Dashboard Page!</div>;
+}
+
 class App extends Component<any, any> {
   render() {
     return (
-      <div className="App">
-        <LoginForm />
-      </div>
+      <Router>
+        <div className="App">
+          <Route path="/">
+            <Menubar model={items} />
+          </Route>
+          <Switch>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route exact path="/">
+              <div className="p-d-flex p-jc-center p-mt-2">
+                <LoginForm redirectUrl="/dashboard" />
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -2,21 +2,76 @@ import React, { Component } from "react";
 import { Button } from "primereact/button";
 import { Password } from "primereact/password";
 import { InputText } from "primereact/inputtext";
+import { Message } from 'primereact/message';
+
+import { Redirect } from "react-router-dom";
+
+import "./LoginForm.scss";
+
+interface OwnProps {
+  username?: string; // browser cached username
+  password?: string; // browser cached password
+  errorMsg?: string; // error message
+  redirectUrl: string; // redirect
+}
+
+interface OwnState {
+  username: string;
+  password: string;
+  errorMsg: string;
+  status: number;
+}
+
+const STATE_INIT = 0;
+const STATE_SUCCESSFUL = 1;
+const STATE_FAILED = 2;
 
 class LoginForm extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      username: props.username || "",
+      password: props.password || "",
+      errorMsg: props.errorMsg || "Invalid username/password.",
+      status: props.status || STATE_INIT,
+    };
+  }
+
+  onSubmit = async () => {
+    this.setState({status: STATE_SUCCESSFUL});
+  };
+
   render() {
     return (
-      <form className="p-fluid">
+      <div>
+      <div className="p-fluid p-mb-2">
+        <Message severity="error" text={this.state.errorMsg}></Message>
+      </div>
+      <div className="p-fluid p-p-5 login-form">
         <div className="p-field">
           <label htmlFor="username">Username</label>
-          <InputText id="username" name="username" type="text" />
+          <InputText
+            name="username"
+            type="text"
+            value={this.state.username}
+            onChange={(e) => this.setState({ username: (e.target as HTMLInputElement).value })}
+          />
         </div>
         <div className="p-field">
           <label htmlFor="password">Password</label>
-          <Password id="password" name="password" type="text" />
+          <Password
+            name="password"
+            type="text"
+            value={this.state.password}
+            onChange={(e) => this.setState({ password: (e.target as HTMLInputElement).value })}
+          />
         </div>
-        <Button label="Sign in" />
-      </form>
+        <Button label="Sign in" onClick={this.onSubmit} />
+        {this.state.status === STATE_SUCCESSFUL && (
+          <Redirect to={this.props.redirectUrl} />
+        )}
+      </div>
+      </div>
     );
   }
 }
